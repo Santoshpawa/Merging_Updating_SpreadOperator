06f91b204e6ede6cb0573b33004b3dfffaa8e161
Santosh Singh Pawar <textingsantosh@gmail.com>
2025-03-01 09:31:34 +0530

    This program demonstrate the use of spread operator in merging and updating an object

---
+++ program.js
@@ -0,0 +1,6 @@
+const profile = { name: "Charlie", age: 29, address: { city: "San Francisco", zipcode: "94101" } };
+
+const updates = { age: 30, address: { zipcode: "94109", country: "USA" } };
+
+let person = {...profile,...updates}
+console.log(JSON.stringify(person,null,4))
